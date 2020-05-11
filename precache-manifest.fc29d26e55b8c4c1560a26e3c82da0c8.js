02278bacb67aff7352ba762ff8368cbe49d9003f
Abdülkadir Karabaş <abdulkadir.karabas@outlook.com>
2020-05-11 18:55:36 +0300

    Updates

---
+++ precache-manifest.fc29d26e55b8c4c1560a26e3c82da0c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202fcf45e666d1d3c1495e7687b6f6ed",
+    "url": "/sketch/index.html"
+  },
+  {
+    "revision": "593826624e6fc1561843",
+    "url": "/sketch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22a4d4b33502b28b94bf",
+    "url": "/sketch/static/js/2.4d38a523.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sketch/static/js/2.4d38a523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593826624e6fc1561843",
+    "url": "/sketch/static/js/main.134a67af.chunk.js"
+  },
+  {
+    "revision": "e27a24dff65549ba318f",
+    "url": "/sketch/static/js/runtime-main.c3275e43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sketch/static/media/logo.5d5d9eef.svg"
+  }
+]);
